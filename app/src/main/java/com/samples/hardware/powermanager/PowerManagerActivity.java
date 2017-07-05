391ee218c7f0f82b1582a05f38c8d0507368a4ad
java2019 <java_2019@mail.ru>
2017-07-05 17:46:06 +0300

    test1

--- app/src/main/java/com/samples/hardware/powermanager/PowerManagerActivity.java
+++ app/src/main/java/com/samples/hardware/powermanager/PowerManagerActivity.java
@@ -1,10 +1,12 @@
 package com.samples.hardware.powermanager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class PowerManagerActivity extends AppCompatActivity
         implements View.OnClickListener{
@@ -16,10 +18,45 @@ public class PowerManagerActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_manager);
+        manager = (PowerManager)getSystemService(
+                Context.POWER_SERVICE);
     }
 
     @Override
     public void onClick(View view) {
-
+        // Освобождаем предыдущий Wake Lock,
+        // если такой существует
+        if (wakeLock != null) {
+            wakeLock.release();
+        }
+        String wlName = "";
+        switch (view.getId()) {
+            case R.id.bPartialWL:
+                wlName = getResources().getString(
+                        R.string.partial_wake_lock);
+                wakeLock = manager.newWakeLock(
+                        PowerManager.PARTIAL_WAKE_LOCK, wlName);
+                break;
+            case R.id.bScreenBrightWL:
+                wlName = getResources().getString(
+                        R.string.screen_bright_wake_lock);
+                wakeLock = manager.newWakeLock(
+                        PowerManager.SCREEN_BRIGHT_WAKE_LOCK, wlName);
+                break;
+            case R.id.bScreenDimWL:
+                wlName = getResources().getString(
+                        R.string.screen_dim_wake_lock);
+                wakeLock = manager.newWakeLock(
+                        PowerManager.SCREEN_DIM_WAKE_LOCK, wlName);
+                break;
+            case R.id.bFullWL:
+                wlName = getResources().getString(
+                        R.string.full_wake_lock);
+                wakeLock = manager.newWakeLock(
+                        PowerManager.FULL_WAKE_LOCK, wlName);
+                break;
+        }
+        Toast.makeText(this, wlName + " ON", Toast.LENGTH_LONG).show();
+        wakeLock.acquire();
     }
 }
