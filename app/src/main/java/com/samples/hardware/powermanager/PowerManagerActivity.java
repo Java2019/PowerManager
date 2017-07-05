2cadbcbd2f083b5473feb92370b6097a44ae4056
java2019 <java_2019@mail.ru>
2017-07-05 17:44:33 +0300

    test1

--- app/src/main/java/com/samples/hardware/powermanager/PowerManagerActivity.java
+++ app/src/main/java/com/samples/hardware/powermanager/PowerManagerActivity.java
@@ -1,13 +1,25 @@
 package com.samples.hardware.powermanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class PowerManagerActivity extends AppCompatActivity {
+public class PowerManagerActivity extends AppCompatActivity
+        implements View.OnClickListener{
+
+    private PowerManager manager;
+    private WakeLock wakeLock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_manager);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
