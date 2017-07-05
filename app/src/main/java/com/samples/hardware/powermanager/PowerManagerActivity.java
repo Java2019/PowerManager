3be90e9163c87c94c92efbb03c41b15f2c63c8c1
java2019 <java_2019@mail.ru>
2017-07-05 17:40:34 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/hardware/powermanager/PowerManagerActivity.java
@@ -0,0 +1,13 @@
+package com.samples.hardware.powermanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PowerManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power_manager);
+    }
+}
